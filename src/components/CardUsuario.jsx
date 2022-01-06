2e9b05cd90d2b118f010e3d13eb505c500adb222
magus <martingramajo08@gmail.com>
2022-01-06 03:29:57 -0300

    agrego la card de usuario

--- src/components/CardUsuario.jsx
+++ src/components/CardUsuario.jsx
@@ -1,37 +1,40 @@
 import { faCamera } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import {  Card, Nav } from "react-bootstrap";
+import { Card, Nav } from "react-bootstrap";
+import Caja1 from "./Caja1";
+import Caja2 from "./Caja2";
+import Caja3 from "./Caja3";
 
 export default function CardUsuario() {
   return (
-    <div className="container">
-      <Card style={{ width: "18rem" }}>
-        <Card.Img
-          className="imgRedonda mt-2"
-          variant="top"
-          src="https://res.cloudinary.com/dtbfspso5/image/upload/v1641442869/12575cb97a22f0f_qrs5bs.jpg"
-          fluid
-        />
-        <div className="d-flex justify-content-end me-5">
-          <FontAwesomeIcon icon={faCamera} />
-        </div>
+    <div className="container d-flex flex-wrap justify-content-around mt-2">
+      <div>
+        <Card className="mt-3" style={{ width: "18rem" }}>
+          <Card.Img
+            className="imgRedonda mt-2"
+            variant="top"
+            src="https://res.cloudinary.com/dtbfspso5/image/upload/v1641442869/12575cb97a22f0f_qrs5bs.jpg"
+          />
+          <div className="d-flex justify-content-end me-5">
+            <FontAwesomeIcon icon={faCamera} />
+          </div>
 
-        <Card.Body>
-          <Card.Title className="text-center">Usuario</Card.Title>
-          <hr />
-          <Card.Text>
-            <div className="d-flex flex-wrap justify-content-around">
-              <Nav.Item>
-                <Nav.Link href="/home">New Mood</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="link-1">Feedback</Nav.Link>
-              </Nav.Item>
-            </div>
-          </Card.Text>
-        </Card.Body>
-      </Card>
+          <Card.Body>
+            <Card.Title className="text-center">Usuario</Card.Title>
+            <hr />
+            <Card.Text className="d-flex flex-wrap justify-content-around">
+                  <Nav.Link href="/home">New Mood</Nav.Link>
+                  <Nav.Link eventKey="link-1">Feedback</Nav.Link>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div>
+        <Caja1 />
+        <Caja2 />
+        <Caja3 />
+      </div>
     </div>
   );
 }
