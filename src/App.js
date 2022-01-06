eba2430f626a60ae7efed69efc28b33d87cb3ba9
magus <martingramajo08@gmail.com>
2022-01-06 04:57:44 -0300

    agrego otro navbar para que opines xoxo cual de los dos te gusta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CardUsuario from './components/CardUsuario';
 import MyNavBar from './components/MyNavBar';
+import MyNavBar2 from './components/MyNavBar2';
 
 function App() {
   return (
     <div >
+      <MyNavBar2 />
       <MyNavBar />
       <CardUsuario />
     </div>
