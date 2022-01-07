f02be6864a5c17be87137bb4e04faf5fc912a255
magus <martingramajo08@gmail.com>
2022-01-07 17:56:41 -0300

    creo las pages del proyecto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MyNavBar from '../components/MyNavBar'
+
+export default function Home() {
+    return (
+        <div>
+            <MyNavBar />
+        </div>
+    )
+}
---
+++ src/pages/Perfil.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardUsuario from '../components/CardUsuario'
+import MyNavLogin from '../components/MyNavLogin'
+
+export default function Perfil() {
+    return (
+        <div>
+            <MyNavLogin />
+            <CardUsuario />
+        </div>
+    )
+}
