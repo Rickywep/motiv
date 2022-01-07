daab13511cb32f61ac09dab21d043690b68525fa
magus <martingramajo08@gmail.com>
2022-01-07 17:58:43 -0300

    agrego funcionalidad al nav

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import {  Button, Container, Image, Nav, Navbar } from "react-bootstrap";
-import ModalLogin from "./ModalLogin";
+import { Button, Container, Image, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
 
 export default function MyNavBar() {
   return (
     <div>
       <Navbar className="fondo-nav" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand to="/" exact as={NavLink}>
             <Image
               style={{ width: "50px" }}
               src="https://res.cloudinary.com/dtbfspso5/image/upload/v1641438758/image_smpczk.png"
               alt=""
             />
           </Navbar.Brand>
-          <Nav className="me-5">
-            <ModalLogin />
-            <span className="text-white mt-2 mx-3">Ricky Moreno</span>
-            <Button  className="color-celeste-claro border-0 mx-2"> cerrar sesión</Button>
-          </Nav>
+          <Button
+            active
+            className="ml-auto color-celeste-claro border-0"
+            to="login"
+            as={NavLink}
+          >
+            Login
+          </Button>
         </Container>
       </Navbar>
     </div>
