49d7bba340d5106ce728b82f32d8d35d3024217b
magus <martingramajo08@gmail.com>
2022-01-11 02:21:38 -0300

    Agrego mas feed de ejemplos

--- src/components/Cajas.jsx
+++ src/components/Cajas.jsx
@@ -21,7 +21,7 @@ const usuarios = [
       "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
     fotoB:
       "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
-    mood: "Divertido",
+    mood: "DIVERTIDO",
     descripcion: "",
     fecha: "12 de abril",
   },
@@ -57,7 +57,7 @@ const usuarios = [
       "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
     fotoB:
       "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
-    mood: "Divertido",
+    mood: "DIVERTIDO",
     descripcion: "",
     fecha: "12 de abril",
   },
@@ -93,12 +93,48 @@ const usuarios = [
       "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
     fotoB:
       "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
-    mood: "Divertido",
+    mood: "DIVERTIDO",
     descripcion: "",
     fecha: "12 de abril",
   },
   {
     id: 9,
+    usuarioA: "Ricky Moreno",
+    usuarioB: "Florencia",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641454166/1618942523622_rz1gof.jpg",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    mood: "BUEN TRABAJO",
+    descripcion: "Hizo un excelente trabajo en el proyecto!",
+    fecha: "12 de abril",
+  },
+  {
+    id: 10,
+    usuarioA: "Rodrigo",
+    usuarioB: "Florencia",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    mood: "AMABLE",
+    descripcion: "Buen Trabajo en Equipo",
+    fecha: "12 de abril",
+  },
+  {
+    id: 11,
+    usuarioA: "Florencia",
+    usuarioB: "Rodrigo",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
+    mood: "DIVERTIDO",
+    descripcion: "",
+    fecha: "12 de abril",
+  },
+  {
+    id: 12,
     usuarioA: "Ricky Moreno",
     usuarioB: "Florencia",
     fotoA:
