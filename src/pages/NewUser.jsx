0b9de5de86dd74b4bd07cd4b7561cecc58d3fd4e
magus <martingramajo08@gmail.com>
2022-01-11 02:03:01 -0300

    agrego pages NewUser

---
+++ src/pages/NewUser.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormNewUser from '../components/FormNewUser'
+import NavAdmin from '../components/NavAdmin'
+
+export default function NewUser() {
+    return (
+        <div>
+            <NavAdmin />
+            <FormNewUser />
+        </div>
+    )
+}
