8a1a3b5a6551446f293c55a8b67189d3d3b09ce7
magus <martingramajo08@gmail.com>
2022-01-11 02:02:10 -0300

    agrego perfiladmin

---
+++ src/components/PerfilAdmin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavAdmin from './NavAdmin'
+
+export default function PerfilAdmin() {
+    return (
+        <div>
+            <NavAdmin />
+        </div>
+    )
+}
