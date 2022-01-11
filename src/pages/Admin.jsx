c7d222bd609e8e559460aed22caf0ae8228dc6f4
magus <martingramajo08@gmail.com>
2022-01-11 02:00:49 -0300

    agrego pages admin y loginadmin

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PerfilAdmin from '../components/PerfilAdmin'
+
+export default function Admin() {
+    return (
+        <div>
+            <PerfilAdmin />
+        </div>
+    )
+}
---
+++ src/pages/LoginAdmin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormAdmin from '../components/FormAdmin'
+import MyNavBar from '../components/MyNavBar'
+
+export default function LoginAdmin() {
+    return (
+        <div>
+            <MyNavBar />
+            <FormAdmin />
+        </div>
+    )
+}
