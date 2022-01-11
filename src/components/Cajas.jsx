743654aae50e1d9286fa6fc8463c22c0b60e0379
magus <martingramajo08@gmail.com>
2022-01-11 01:58:43 -0300

    hago el mapeo de cada caja

--- src/components/Cajas.jsx
+++ src/components/Cajas.jsx
@@ -1,14 +1,122 @@
 import React from "react";
-import Caja1 from "./Caja1";
-import Caja2 from "./Caja2";
-import Caja3 from "./Caja3";
+import Caja from "./Caja";
+const usuarios = [
+  {
+    id: 1,
+    usuarioA: "Rodrigo",
+    usuarioB: "Florencia",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    mood: "AMABLE",
+    descripcion: "Buen Trabajo en Equipo",
+    fecha: "12 de abril",
+  },
+  {
+    id: 2,
+    usuarioA: "Florencia",
+    usuarioB: "Rodrigo",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
+    mood: "Divertido",
+    descripcion: "",
+    fecha: "12 de abril",
+  },
+  {
+    id: 3,
+    usuarioA: "Ricky Moreno",
+    usuarioB: "Florencia",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641454166/1618942523622_rz1gof.jpg",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    mood: "BUEN TRABAJO",
+    descripcion: "Hizo un excelente trabajo en el proyecto!",
+    fecha: "12 de abril",
+  },
+  {
+    id: 4,
+    usuarioA: "Rodrigo",
+    usuarioB: "Florencia",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    mood: "AMABLE",
+    descripcion: "Buen Trabajo en Equipo",
+    fecha: "12 de abril",
+  },
+  {
+    id: 5,
+    usuarioA: "Florencia",
+    usuarioB: "Rodrigo",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
+    mood: "Divertido",
+    descripcion: "",
+    fecha: "12 de abril",
+  },
+  {
+    id: 6,
+    usuarioA: "Ricky Moreno",
+    usuarioB: "Florencia",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641454166/1618942523622_rz1gof.jpg",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    mood: "BUEN TRABAJO",
+    descripcion: "Hizo un excelente trabajo en el proyecto!",
+    fecha: "12 de abril",
+  },
+  {
+    id: 7,
+    usuarioA: "Rodrigo",
+    usuarioB: "Florencia",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    mood: "AMABLE",
+    descripcion: "Buen Trabajo en Equipo",
+    fecha: "12 de abril",
+  },
+  {
+    id: 8,
+    usuarioA: "Florencia",
+    usuarioB: "Rodrigo",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447344/icon-5359553_960_720_ljpyfq.png",
+    mood: "Divertido",
+    descripcion: "",
+    fecha: "12 de abril",
+  },
+  {
+    id: 9,
+    usuarioA: "Ricky Moreno",
+    usuarioB: "Florencia",
+    fotoA:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641454166/1618942523622_rz1gof.jpg",
+    fotoB:
+      "https://res.cloudinary.com/dtbfspso5/image/upload/v1641447343/icon-5359554_960_720_dmlzhr.png",
+    mood: "BUEN TRABAJO",
+    descripcion: "Hizo un excelente trabajo en el proyecto!",
+    fecha: "12 de abril",
+  },
+];
 
 export default function Cajas() {
   return (
-    <div className="cajaMobile cajacomentario">
-      <Caja1 />
-      <Caja2 />
-      <Caja3 />
+    <div className="cajaMobile cajacomentario scroll">
+      {usuarios.map((usuario, id) => (
+        <Caja key={id} data={usuario} />
+      ))}
     </div>
   );
 }
