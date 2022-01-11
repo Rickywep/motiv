feb8dbcf8d9acd5025220f4ac4bc518d9fbee2da
magus <martingramajo08@gmail.com>
2022-01-11 02:03:58 -0300

    creo las rutas de admin, newUser, newMood y loginAdmin

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import {
 import Login from "./pages/Login";
 import Perfil from './pages/Perfil';
 import Home from './pages/Home';
+import Admin from './pages/Admin';
+import LoginAdmin from './pages/LoginAdmin';
+import NewUser from './pages/NewUser';
+import NewMood from './pages/NewMood';
 
 export default function App() {
   return (
@@ -21,6 +25,18 @@ export default function App() {
         </Route>
         <Route path="/Perfil" >
           <Perfil />
+        </Route>
+        <Route path="/loginAdmin" >
+          <LoginAdmin />
+        </Route>
+        <Route path="/Admin" >
+          <Admin />
+        </Route>
+        <Route path="/NewUser" >
+          <NewUser />
+        </Route>
+        <Route path="/NewMood" >
+          <NewMood />
         </Route>
       </Switch>
     </Router>
