3fa8ffcbb8d534d8fba48ff2bc1173c6c5562017
magus <martingramajo08@gmail.com>
2022-01-11 02:03:11 -0300

    agrego Pages NewMood

---
+++ src/pages/NewMood.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavAdmin from '../components/NavAdmin'
+
+export default function NewMood() {
+    return (
+        <div>
+            <NavAdmin />
+             <h1 className='text-center mt-2'> Nuevo Mood</h1>
+        </div>
+    )
+}
