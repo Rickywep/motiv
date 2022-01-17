9f2d0d8d1d50ace7fcbedbc048b49a068152ba71
magus <martingramajo08@gmail.com>
2022-01-17 13:07:23 -0300

    quito el nav de la page login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Formulario from '../components/Formulario'
-import MyNavBar from '../components/MyNavBar'
+
 
 export default function Login() {
     return (
         <div>
-            <MyNavBar />
             <Formulario />
         </div>
     )
